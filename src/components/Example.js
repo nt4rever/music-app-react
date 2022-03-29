20517b5ace4aa6952b867e15950440c0087960fa
nt4rever <levantanald@gmail.com>
2022-03-29 23:44:00 +0700

    optimize

--- src/components/Example.js
+++ src/components/Example.js
@@ -2,11 +2,12 @@ import React, { useEffect, useState } from 'react';
 import YouTube from 'react-youtube';
 
 function Example() {
-    const [videoId, setVideoId] = useState(null);
+    const [videoId, setVideoId] = useState("fzu4VT-QWK4");
     const [title, setTitle] = useState(null);
     const [player, setPlayer] = useState(null);
     const [timer, setTimer] = useState(null)
     const [isMounted, setIsMounted] = useState(false)
+    const [isPlay, setIsPlay] = useState(false);
 
     async function updateVideoId() {
         try {
@@ -14,8 +15,9 @@ function Example() {
             const data = await result.json()
             setVideoId(data.url)
             setTitle(data.name)
+            console.clear()
         } catch (e) {
-            console.error(e)
+            // console.error(e)
         }
         clearTimeout(timer)
         setTimer(setTimeout(updateVideoId, 1000))
@@ -33,16 +35,16 @@ function Example() {
     };
 
     const onPlayVideo = () => {
+        setIsPlay(!isPlay)
         player.playVideo();
     };
 
     const onPauseVideo = () => {
+        setIsPlay(!isPlay)
         player.pauseVideo();
     };
 
     const opts = {
-        // height: '500',
-        // width: '700',
         playerVars: {
             autoplay: 1,
         },
@@ -56,12 +58,17 @@ function Example() {
                     <YouTube videoId={videoId} onReady={onReady} opts={opts} />
                 </div>
                 <div className='control'>
-                    <button type="button" className='button is-primary mr-2' onClick={onPlayVideo} disabled={!player}>
-                        Play
-                    </button>
-                    <button type="button" className='button is-danger' onClick={onPauseVideo} disabled={!player}>
-                        Pause
-                    </button>
+                    {
+                        isPlay ? (
+                            <button type="button" className='button is-danger' onClick={onPauseVideo} disabled={!player}>
+                                Pause
+                            </button>
+                        ) : (
+                            <button type="button" className='button is-primary mr-2' onClick={onPlayVideo} disabled={!player}>
+                                Play
+                            </button>
+                        )
+                    }
                 </div>
             </div>
         </div>
